30f3a530afa511fa38c1789bd673bb84c8e27081
Mizner <mmizner@pyxl.com>
2019-01-13 17:38:22 -0500

    remove excessive browswerlist

--- babel.config.js
+++ babel.config.js
@@ -4,13 +4,6 @@ module.exports = {
     [
       '@babel/env',
       {
-        targets: {
-          edge: '17',
-          firefox: '60',
-          chrome: '67',
-          safari: '11.1',
-          ie: '11'
-        },
       }
     ]
   ]
